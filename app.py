87b6d21d4df9c8e15a3b62de27b63538d8e14506
Hema0304 <hemaselvam0304.july@gmail.com>
2026-05-02 08:33:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ##RAG Q&A conversation with PDF including chat history
 import streamlit as st 
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
